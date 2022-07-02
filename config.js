05fcdde65f3b6bbf350edf44451f090015be2514
Toshi さん <91844428+Toshiro-sama@users.noreply.github.com>
2022-07-02 13:58:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ global.port = process.env.PORT || 8080;
   (global.sessionName = "arus"),
   (global.name = process.env.NAME || "Mizuhara"),
   (global.menu =
-    "🎫 Mizuhara's Command Liste* 🎫\n\n💻 *Dev*\n❐ ```update, broadcast, ban, unban, chatid, session, join```\n\n🔮 *General*\n❐ ```help, delete, info, rank, profile```\n\n💠 *Fun*\n❐ ```ship, advice, quote, leaderboard, react, fact, bot```\n\n🔈 *Media*\n❐ ```yta, ytv, play, gify, yts, lyrics```\n\n👑 *Moderation*\n❐ ```demote, remove, ping, promote, deact, act, invite, group, seticon, setdesc```\n\n💮 *Weeb*\n❐ ```anime, manga, charecter, haigusha, neko, waifu, pokemon```\n\n🖇 *Utils*\n❐ ```google, image, meme, subreddit, tourl, img, iguser, gif, sticker, take, pinterest, carbon```\n\n🗃️ *Note: *Calls and Dm will lead you to ban*"),
+    "🏮 Mizuhara's Command List* 🏮\n\n*━━━━『 Dev 』━━━━*\n❐ ```update, broadcast, ban, unban, chatid, session, join```\n\n*━━━━『 General 』━━━━*\n❐ ```help, delete, info, rank, profile```\n\n*━━━━『 Fun 』━━━━*\n❐ ```ship, advice, quote, leaderboard, react, fact, bot```\n\n*━━━━『 Media 』━━━━*\n❐ ```yta, ytv, play, gify, yts, lyrics```\n\n*━━━━『 Moderation 』━━━━*\n❐ ```demote, remove, ping, promote, deact, act, invite, group, seticon, setdesc```\n\n*━━━━『 Weeb 』━━━━*\n❐ ```anime, manga, charecter, haigusha, neko, waifu, pokemon```\n\n*━━━━『 Utils 』━━━━*\n❐ ```google, image, meme, subreddit, tourl, img, iguser, gif, sticker, take, pinterest, carbon```\n\n *🕊️ 𝙏𝙝𝙚 𝘽𝙚𝙨𝙩 𝙞𝙨 𝙔𝙚𝙩 𝙩𝙤 𝘽𝙚 価*"),
   (global.thumb = fs.readFileSync("./src/info.jpeg"));
 
 let file = require.resolve(__filename);
