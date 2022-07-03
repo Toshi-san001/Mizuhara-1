5a5aa2fb158cfdfb8ad18460b178f3ef0fb6ecdd
Toshi さん <91844428+Toshiro-sama@users.noreply.github.com>
2022-07-03 11:59:01 +0530

    Update brain.js

--- brain.js
+++ brain.js
@@ -244,7 +244,7 @@ break
 			const hlp=`
  *❤️(◍•ᴗ•◍)❤Kon'nichiwa 👋🏻 ${pushname} Senpai I'm Mizuhara* 
 
-🎐My Prefix is: = 
+🎐My Prefix is: ${prefix}
 
 📪 My all usable commands are listed below 📪 
 
