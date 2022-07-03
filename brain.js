d3201a7fb10a2a86447567d77cebdc3d46eae44a
Toshi さん <91844428+Toshiro-sama@users.noreply.github.com>
2022-07-04 05:23:00 +0530

    Update brain.js

--- brain.js
+++ brain.js
@@ -171,8 +171,8 @@ case '':
 
 
     const dbut = [
-{buttonId: `${prefix}help`, buttonText: {displayText: '🚀Commands'}, type: 1}
-{buttonId: `${prefix}info`, buttonText: {displayText: '🎐Bot status'}, type: 1}
+{buttonId: '=help', buttonText: {displayText: '🚀 Help'}, type: 1},
+{buttonId: '=info', buttonText: {displayText: '🍁 Info'}, type: 1}
 ]
 let buttonMessaged = {
         image: {url:"https://telegra.ph/file/5701d54c0e3e6f51aa161.jpg"},
