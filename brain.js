f7edc6a1adc81bf3de8d85c2b255dd46d6e1842e
Toshi さん <91844428+Toshiro-sama@users.noreply.github.com>
2022-07-05 14:31:52 +0530

    Update brain.js

--- brain.js
+++ brain.js
@@ -283,11 +283,12 @@ let hbutto = {
         footer: '©Arus 2022',
         buttons: hhbut,
         headerType: 4
-        contextInfo:{externalAdReply:{
+        contextInfo:{"externalAdReply":{
         title: "Mizuhara by Arus",
         body": "𝐌𝐢𝐳𝐮𝐡𝐚𝐫𝐚",
         thumbnail": await getBuffer("https://i.pinimg.com/736x/01/bd/31/01bd31fb1b185e38d9f605b0f8f16b90.jpg"),
         sourceUrl": "https://kanojo-okarishimasu.fandom.com/wiki/Chizuru_Ichinose"}}}, { quoted: m})
+}}
     }
 arus.sendMessage(m.chat,hbutto,{quoted:m})
 arus.sendMessage(m.chat, { text: help, contextInfo:{"externalAdReply": {"title": `WhatsApp-Botto`,"body": `𝐌𝐢𝐳𝐮𝐡𝐚𝐫𝐚`, "previewType": "PHOTO","thumbnailUrl": ``,"thumbnail": await getBuffer("https://i.pinimg.com/736x/01/bd/31/01bd31fb1b185e38d9f605b0f8f16b90.jpg"),"sourceUrl": "https://kanojo-okarishimasu.fandom.com/wiki/Chizuru_Ichinose"}}}, { quoted: m})
