644c90da2a96e512b5628fc80ae31af863cb3671
Toshi さん <91844428+Toshiro-sama@users.noreply.github.com>
2022-07-05 14:53:33 +0530

    Update brain.js

--- brain.js
+++ brain.js
@@ -274,8 +274,8 @@ break
 
  🎐 𝙋𝙤𝙬𝙚𝙧𝙚𝙙 𝙗𝙮 𝘼𝙧𝙪𝙨 🎐`			     
 const hhbut = [
-{buttonId: '=info', buttonText: {displayText: ' Info'}, type: 1},
-{buttonId: '=profile', buttonText: {displayText: ' Profile'}, type: 1}
+{buttonId: `${prefix}info`, buttonText: {displayText: '⭐Info'}, type: 1},
+{buttonId: `${prefix}profile`, buttonText: {displayText: '🎋Profile'}, type: 1}
 ]
 let hbutto = {
         file: arus.sendMessage(m.chat,{video:fs.readFileSync('./src/help.mp4'),gifPlayback:true,caption:hlp},{quoted:m}),
