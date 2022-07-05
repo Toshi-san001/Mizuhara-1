b7c90d1bce15b0011cdeecf08626e948c0ae2cd6
Toshi さん <91844428+Toshiro-sama@users.noreply.github.com>
2022-07-06 00:51:37 +0530

    Update brain.js

--- brain.js
+++ brain.js
@@ -419,7 +419,6 @@ let buttonMessagem = {
         title: "𒈒Arus𒈒",
         body: "Arus Modeators",
         thumbnail: await getBuffer("https://telegra.ph/file/5b28cd69c83532a9cd669.jpg"),
-}}
     }
 
  await arus.sendMessage(m.chat,buttonMessagem,{quoted:m})
