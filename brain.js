c55e56635b984707eecb07fb8cc5e65676f71d97
Toshi さん <91844428+Toshiro-sama@users.noreply.github.com>
2022-07-11 23:24:32 +0530

    Update brain.js

--- brain.js
+++ brain.js
@@ -262,10 +262,8 @@ const listMessage = {
 }
                     await arus.sendMessage(m.chat, listMessage)
 
- }
    }
-arus.sendMessage(m.chat, { text: help, contextInfo:{"externalAdReply": {"title": `WhatsApp-Botto`,"body": `𝐌𝐢𝐳𝐮𝐡𝐚𝐫𝐚`, "previewType": "PHOTO","thumbnailUrl": ``,"thumbnail": await getBuffer("https://i.pinimg.com/736x/01/bd/31/01bd31fb1b185e38d9f605b0f8f16b90.jpg"),"sourceUrl": "https://kanojo-okarishimasu.fandom.com/wiki/Chizuru_Ichinose"}}}, { quoted: m})
-			   break
+break
 		   case 'session': {
 			   if (!isCreator) return m.reply("📍The user of this command must be the owner of the bot")
 			   const session = require(`./${sessionName}.json`)
